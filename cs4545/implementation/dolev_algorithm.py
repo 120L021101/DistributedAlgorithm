a1bd7969096b9cded2800709e45751e9da4af095
HenriqueLin <henrique.lin99@gmail.com>
2024-11-17 19:14:51 +0100

    Implement optimization

--- cs4545/implementation/dolev_algorithm.py
+++ cs4545/implementation/dolev_algorithm.py
@@ -16,17 +16,18 @@ class DolevAlgorithm(DistributedAlgorithm):
     Args:
         DistributedAlgorithm (_type_): _description_
     """
+    @staticmethod
+    def formatPath(path):
+        nodes = [i for i in range(path.bit_length()) if (path >> i) & 1]
+        return '[' + ', '.join(map(str, nodes)) + ']'
 
     @staticmethod
     def formatPaths(paths):
-        return '| '.join([
-            '-'.join([str(i) for i, bit in enumerate(reversed(bin(path))) if bit == "1"])
-            if path != 0 else "empty"
-                    for path in paths
-        ])
-    @staticmethod
-    def formatPath(path):
-        return '-'.join([str(i) for i, bit in enumerate(reversed(bin(path))) if bit == "1"]) if path != 0 else "empty"
+        return '[' + ', '.join([
+            str(DolevAlgorithm.formatPath(path))
+            for path in paths
+        ]) + ']'
+
 
     # upon event ⟨Dolev, Init⟩ 
     def __init__(self, settings: CommunitySettings) -> None:
@@ -34,8 +35,10 @@ class DolevAlgorithm(DistributedAlgorithm):
         super().__init__(settings)
 
         self.delivered = dict()
+        self.delivered_neighbors = dict()
         self.paths = dict()
         self.sent_msg_cnt = 0
+        self.seq_id = 0
 
         # 方便测试，针对latency和msg复杂度
         self.interface_recv_msg_cnt = 0
@@ -53,6 +56,8 @@ class DolevAlgorithm(DistributedAlgorithm):
     # only starter does "Broadcast"
     async def on_start_as_starter(self, message=None):
         self.start_time = time.time()
+        seq_id = self.seq_id
+        seq_id += 1 
         print(f"Node {self.node_id} is starting the Delov algorithm")
         if message is None:
             message = f"Hello From {self.node_id}"
@@ -92,6 +97,8 @@ class DolevAlgorithm(DistributedAlgorithm):
     def criteria(self, bit_paths: set) -> bool:
         # check if exists exactly f + 1 vertex disjoint paths
         print(f"[Node {self.node_id}] check disjoint: {self.formatPaths(bit_paths)}")
+        if(len(bit_paths) < self.f +1):
+            return False
         bit_paths = list(bit_paths)
         return self.__dfs_max_disjoint(0, [0 for _ in range(len(bit_paths))],
                                        bit_paths, 0) >= self.f + 1
@@ -127,60 +134,79 @@ class DolevAlgorithm(DistributedAlgorithm):
             
         receiver_id = self.node_id
         sender_id, m, m_id, received_path, source_id = payload.sender_id, payload.message, payload.message_id, payload.path, payload.source_id
-
+        seq_id = self.seq_id
+        self.seq_id += 1
 
         # initialize variables if needed
         if m_id not in self.paths:
             self.paths[m_id] = set()
         if m_id not in self.delivered:
             self.delivered[m_id] = False
-        print(f"[Node {receiver_id}] Got a message {m} source of {source_id} with sender {sender_id}.\t" + 
+            self.delivered_neighbors[m_id] = 0
+        print(f"[Node {receiver_id}:{seq_id}] Got a message {m} source of {source_id} with sender {sender_id}.\t" + 
                 f"msg_id: {m_id}, msg path: {self.formatPath(received_path)} " +
-                                 f"and pi paths: {self.formatPaths(self.paths[m_id])}")
+                                 f"and local paths: {self.formatPaths(self.paths[m_id])}")
         self.interface_recv_msg_cnt += 1
+       
+        # MD5
+        if(self.MD5 and self.delivered[m_id]):
+            print(f"[Node {receiver_id}:{seq_id}] received a msg of a delivered msg, no futher processing")
+            return
         
-
+        # MD3
+        if(self.MD3 and received_path == 0):
+            self.delivered_neighbors[m_id] = self.delivered_neighbors[m_id] | (0x1 << sender_id)
+            print(f"[Node {receiver_id}:{seq_id}] received msg with empty path, current delivered neighbors: {self.formatPath(self.delivered_neighbors[m_id])}")
+            
+        # MD4
+        if(self.MD4 and self.delivered_neighbors[m_id] & received_path != 0):
+            print(f"[Node {receiver_id}:{seq_id}] received path contain neighbors that already delivered, no futher processing")
+            return
+        
         try:
             updated_path = received_path
             if sender_id != source_id:
                 updated_path = received_path | (0x1 << sender_id)
                 
-            self.paths[m_id].add(updated_path)
-            
+            self.paths[m_id].add(updated_path)     
+                   
             # MD1
-            print(self.MD1)
             if not self.is_byzantine and self.MD1 and (sender_id == source_id and not self.delivered[m_id]):
-                print(f"[Node {receiver_id}] Delivered Message: [{m_id}]:{m}")
+                print(f"[Node {receiver_id}:{seq_id}] sender = source, Delivered Message: [{m_id}]:{m}")
                 self.delivered[m_id] = True
                 self.protocol_delivered_msg_cnt += 1
+                
             # byzantine node deliver doesn't count
             if not self.is_byzantine and not self.delivered[m_id] and self.criteria(self.paths[m_id]):
                 # Delivered
-                print(f"[Node {receiver_id}] Delivered Message: [{m_id}]:{m}")
+                print(f"[Node {receiver_id}:{seq_id}] meet f+1 disjoint criteria, Delivered Message: [{m_id}]:{m}")
                 self.delivered[m_id] = True
                 self.protocol_delivered_msg_cnt += 1
 
+            
+            # MD2 task1, send empty path, so updated_path should be empty
+            # MD2 task2, broadcast to all neighbors, so self.paths[m_id] should be empty
+            if self.MD2 and self.delivered[m_id]:
+                updated_path = 0
+                self.paths[m_id] = set()
+            
+            
             # do not send to occurred neighbors
-            merged_path = received_path | (0x1 << source_id) | (0x1 << sender_id)
-            
-            # # MD2 task1, send empty path, so updated_path should be empty
-            # # MD2 task2, broadcast to all neighbors, so merged_path should be empty
-            # if self.MD2 and self.delivered[m_id]:
-            #     updated_path = 0
-            #     merged_path = 0
-            
-            # pi_bit = 0x1 << receiver_id
+            no_sending_node = received_path | (0x1 << source_id) | (0x1 << sender_id)
+            # MD3
+            if(self.MD3): 
+                no_sending_node = no_sending_node | self.delivered_neighbors[m_id]
             for neighbor in self.get_peers():
                 neighbor_id = self.node_id_from_peer(peer=neighbor)
                 neighbor_bitmask = 0x1 << neighbor_id
-                if 0 != (neighbor_bitmask & merged_path):
+                if 0 != (neighbor_bitmask & no_sending_node):
                     continue
-                print(f"[Node {receiver_id}] Send to {neighbor_id}")  
+                print(f"[Node {receiver_id}:{seq_id}] Send msg {m_id} with path {self.formatPath(updated_path)} to {neighbor_id}")  
                 await self.send_with_delay(neighbor, DolevMessage(source_id, receiver_id, m, m_id, updated_path))
                 self.interface_sent_msg_cnt += 1
 
         except Exception as e:
-            print(f"[Node {receiver_id}] Error in al_deliver: {e}")
+            print(f"[Node {receiver_id}:{seq_id}] Error in al_deliver: {e}")
             raise e
 
         # log output to "output/node{node_id}.log"
